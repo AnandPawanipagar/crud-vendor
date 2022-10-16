ee24bd1665a79a940b8c3331d2844ae2887e07b7
AnandPawanipagar <anandpawanipagar97@gmail.com>
2022-10-16 23:43:51 +0530

    feat: Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
-require('dotenv').config();
+require("dotenv").config();
+const connectDatabase = require("./config/database");
+
+// Connecting database
+connectDatabase();
 app.listen(process.env.PORT, () => {
   console.log(`Server is working on localhost:${process.env.PORT}`);
 });
