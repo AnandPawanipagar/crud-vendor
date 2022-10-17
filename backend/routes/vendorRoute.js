e350be1f438657aa60ed214d2c3bd22e190bbca6
AnandPawanipagar <anandpawanipagar97@gmail.com>
2022-10-17 12:15:08 +0530

    feat: routes need to check

--- backend/routes/vendorRoute.js
+++ backend/routes/vendorRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { getAllVendors, createVendor } = require("../controllers/VendorController");
+const { getAllVendors, createVendor, updateVendor } = require("../controllers/VendorController");
 const userRouter = express.Router();
 userRouter.route("/vendors").get(getAllVendors);
 userRouter.route('/vendor/create-vendor').post(createVendor)
+userRouter.route('/vendor/:id').put(updateVendor)
 module.exports = userRouter;
